00376a7d37b808f0ef9e8406f36652de62af6c5c
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-12-21 12:01:51 +0700

    feat(exercise): add completed exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [stageDefault, setCounter] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{stageDefault}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button
+          onClick={() => {
+            setCounter("สวัสดี!");
+          }}
+        >
+          สวัสดี!
+        </button>
+        <button
+          onClick={() => {
+            setCounter("Hi!");
+          }}
+        >
+          Hi!
+        </button>
+        <button
+          onClick={() => {
+            setCounter("你好!");
+          }}
+        >
+          你好!
+        </button>
       </div>
     </div>
   );
